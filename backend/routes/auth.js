8f1713ee8e743f333a340d911e6cff99ccbd2248
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-09-15 20:22:36 +0530

    feat: initialize backend with JWT authentication, user registration, and MongoDB connection
    - Added package.json for project dependencies and scripts
    - Implemented user registration route in auth.js with password hashing
    - Set up Express server in server.js with middleware for JSON parsing, cookie handling, and CORS
    - Connected to MongoDB using Mongoose

---
+++ backend/routes/auth.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/Users.js";
+
+const router = express.Router();
+
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+
+
+const app = express();
+
+// middlewears
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+app.use("/api/auth", authRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
